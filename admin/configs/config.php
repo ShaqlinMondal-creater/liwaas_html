f61061883a6e57f9e0c19701519edfa9c07e192e
Shaqlin <shaqlinmondal8905@gmail.com>
2026-01-13 13:16:57 +0530

    ..

--- admin/configs/config.php
+++ admin/configs/config.php
@@ -4,7 +4,7 @@ return [
   // 'API_BASE_URL' => 'http://192.168.0.105:8000',  // Home Town
   // 'API_BASE_URL' => 'http://192.168.0.101:8000',  //ROOM
   // 'API_BASE_URL' => 'http://192.168.1.3:8000',  // OFFICE 
-  'API_BASE_URL' => 'http://127.0.0.1:8000',  // OFFICE
+  'API_BASE_URL' => 'https://api.liwaas.com',  // LIVE
   'BASE_NAME' => 'Liwaas',
   'BASE_LOGO'    => '../assets/brand/liwaas_logo_white_png.png',
   'BASE_FAV_ICON'    => '../assets/brand/fav_icon.png',

25d9b0ab9fad927ba5eb0300e13d3a4158707f9b
Shaqlin <shaqlinmondal8905@gmail.com>
2026-02-02 21:47:07 +0530

    ..

--- admin/nxt-pages/product-table.php
+++ admin/nxt-pages/product-table.php
@@ -422,10 +422,26 @@ function renderTable(products) {
 
                 <!-- Actions -->
                 <td class="text-center">
-                    <a href="nxt-pages/update-product.php?name=${product.slug}"
-                       class="btn btn-sm btn-light">
-                        Edit
-                    </a>
+                    <div class="menu flex-inline" data-menu="true">
+                        <div class="menu-item" data-menu-item-offset="0, 10px"
+                            data-menu-item-placement="bottom-end"
+                            data-menu-item-placement-rtl="bottom-start"
+                            data-menu-item-toggle="dropdown"
+                            data-menu-item-trigger="click|lg:click">
+                            <button class="menu-toggle btn btn-sm btn-icon btn-light btn-clear">
+                                <i class="ki-filled ki-dots-vertical"></i>
+                            </button>
+                            <div class="menu-dropdown menu-default w-full max-w-[175px]" data-menu-dismiss="true">
+                                <div class="menu-item"><a class="menu-link" href="#"><i class="ki-filled ki-search-list me-2"></i>View</a></div>
+                                <div class="menu-item"><a class="menu-link" href="#"><i class="ki-filled ki-file-up me-2"></i>Export</a></div>
+                                <div class="menu-separator"></div>
+                                <div class="menu-item"><a class="menu-link" href="nxt-pages/update-product.php?name=${product.slug}"><i class="ki-filled ki-pencil me-2"></i>Edit</a></div>
+                                <div class="menu-item"><a class="menu-link" href="#"><i class="ki-filled ki-copy me-2"></i>Make a copy</a></div>
+                                <div class="menu-separator"></div>
+                                <div class="menu-item"><a class="menu-link text-danger" href="#"><i class="ki-filled ki-trash me-2"></i>Remove</a></div>
+                            </div>
+                        </div>
+                    </div>
                 </td>
             </tr>
         `;

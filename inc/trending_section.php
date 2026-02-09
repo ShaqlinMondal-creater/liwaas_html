3598a2b71dad4c4cbee06d0f3363b3f4eddf954c
Shaqlin <shaqlinmondal8905@gmail.com>
2026-02-09 21:06:35 +0530

    ..

--- inc/trending_section.php
+++ inc/trending_section.php
@@ -328,7 +328,7 @@
                   const sizeBtn = document.createElement('button');
                   sizeBtn.textContent = v.size;
                   sizeBtn.className =
-                    "px-4 py-2 border rounded-md text-xs hover:bg-black hover:text-white transition";
+                    "px-2 py-2 border rounded-md text-xs hover:bg-black hover:text-white transition";
 
                   if (index === 0) {
                     sizeBtn.classList.add("bg-black", "text-white");

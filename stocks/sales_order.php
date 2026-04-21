16588f24efbba497a77aea17bff4b7a070c9f98a
Shaqlin <shaqlinmondal8905@gmail.com>
2026-04-22 00:58:10 +0530

    ..

--- stocks/sales_order.php
+++ stocks/sales_order.php
@@ -168,7 +168,6 @@
                         <th class="px-3 py-2">Qty</th>
                         <th class="px-3 py-2">Price</th>
                         <th class="px-3 py-2">Tax</th>
-                        <th class="px-3 py-2">Status</th>
                         <th class="px-3 py-2">Action</th>
                     </tr>
                 </thead>
@@ -454,7 +453,7 @@
         `).join("");
 
         return `
-        <tr class="${item.status === 'inprocess' ? 'bg-orange-50' : ''}">
+        <tr class="${item.status === 'inprocess' ? 'bg-orange-100' : ''}">
             <td>
                 <select class="edit_uid border px-2 py-1">
                     ${options}
@@ -464,17 +463,6 @@
             <td><input value="${item.qty || 1}" class="edit_qty border w-16"></td>
             <td><input value="${item.price || 0}" class="edit_price border w-20"></td>
             <td><input value="${item.tax || 0}" class="edit_tax border w-16"></td>
-            <td class="px-3 py-2">
-                ${
-                    item.status === "inprocess"
-                    ? `<span class="px-2 py-1 text-xs bg-orange-100 text-orange-700 rounded">
-                            Returning
-                    </span>`
-                    : `<span class="px-2 py-1 text-xs bg-green-100 text-green-700 rounded">
-                            Active
-                    </span>`
-                }
-            </td>
             <td class="space-x-2">
                 <button onclick="removeRow(this)" class="text-red-600">✕</button>
 
